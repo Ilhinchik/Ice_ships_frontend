56ce2607c37e4c99efa653294bd19b6bfc3f5302
Ilha <markovila539@gmail.com>
2024-12-03 00:33:36 +0300

    test

--- my-ice-ships/vite.config.ts
+++ my-ice-ships/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import {VitePWA, VitePWAOptions} from "vite-plugin-pwa";
+
+
 const manifestForPlugin: Partial<VitePWAOptions> = {
     registerType: 'autoUpdate',
     includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
@@ -30,7 +32,6 @@ export default defineConfig({
     react(),
     VitePWA(manifestForPlugin)
   ],
-  base: "/Ice_ships_frontend",
   server: {
       port: 3000,
       proxy: {
